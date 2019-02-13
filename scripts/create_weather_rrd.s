99a9ad5906aa50469f4f9aa9df28b638cb6598ea
Brett Hannigan <hanniganbrett@gmail.com>
2019-02-12 20:32:33 -0800

    Test without rainfall gauge (correction)

--- scripts/create_weather_rrd.s
+++ scripts/create_weather_rrd.s
@@ -8,7 +8,7 @@ DS:temperature:GAUGE:1200:-40:50 \
 DS:humidity:GAUGE:1200:0:100 \
 DS:wind_speed:GAUGE:1200:0:30 \
 DS:wind_gust:GAUGE:1200:0:30 \
-DS:rainfall:DCOUNTER:1200:0:100 \
+DS:wind_direction:GAUGE:1200:0:359 \
 RRA:AVERAGE:0.5:1:105120 \
 RRA:MIN:0.5:12:87600 \
 RRA:MAX:0.5:12:387600 \
